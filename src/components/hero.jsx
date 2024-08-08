80ea8f39e9555e6c6fcff0e79787fabb16f7046d
Diego <34121369+Diego-dot-web@users.noreply.github.com>
2024-08-07 22:20:18 -0500

    add nav and slider sorry xd

---
+++ src/components/hero.jsx
@@ -0,0 +1,59 @@
+export default function Hero() {
+  return (
+    <>
+      <nav className="h-10 absolute *:list-none flex justify-around w-full items-center p-4 *:text-lg ">
+        <img
+          src="src/assets/Logo.png"
+          alt="Logo de Vital Pets"
+          className="relative size-36 top-10"
+        />
+        <div className="flex items-center pt-12 gap-4">
+          <a
+            href="#"
+            className="flex justify-center flex-col items-center gap-1 hover:text-red-700"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-home"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+              <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+              <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+            </svg>
+            <li>Inicio</li>
+          </a>
+          <a
+            href="#"
+            className="flex justify-center flex-col items-center gap-1 hover:text-red-700 "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-phone"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+            </svg>
+            <li>Contáctenos</li>
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+}
