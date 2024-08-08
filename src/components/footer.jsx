c02543114f40e7f7046162ed0c818c6720294bb0
Diego <34121369+Diego-dot-web@users.noreply.github.com>
2024-08-08 09:34:55 -0500

    link footer bordes slider

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,18 +3,18 @@ export default function Footer() {
     <footer className="bg-blue-700  shadow w-full dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-white sm:text-center dark:text-gray-400">
-          <a href="#" className="hover:underline">
+          <a href="/" className="hover:underline">
             Vital pets Central de Urgencias
           </a>
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">
+            <a href="/" className="hover:underline me-4 md:me-6">
               Inicio
             </a>
           </li>
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">
+            <a href="/contacto" className="hover:underline me-4 md:me-6">
               Contáctenos
             </a>
           </li>
