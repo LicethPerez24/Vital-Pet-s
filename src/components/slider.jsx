5d08ef3ea7320d9a87b0b5b9904f4bffa8a2424d
Diego <34121369+Diego-dot-web@users.noreply.github.com>
2024-08-08 13:19:43 -0500

    finally working

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -2,8 +2,8 @@ import { Carousel } from "flowbite-react";
 
 export function Component() {
   return (
-    <div className="h-[40rem] sm:h-64 xl:h-80 2xl:h-[40rem]">
-      <Carousel slideInterval={700}>
+    <div className="rounded-none h-[40rem] sm:h-64 xl:h-80 2xl:h-[40rem]">
+      <Carousel slideInterval={1800}>
         <img src="./Vet1.webp" alt="..." />
         <img src="./Vet2.webp" alt="..." />
         <img src="./Vet3.webp" alt="..." />
